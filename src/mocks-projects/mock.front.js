6d60bb88501eea27906028b811fbfd972aec4650
Edmilson Corrêa <edm.correa@hotmail.com>
2022-12-28 23:51:02 -0300

    Adding mock-front-end

--- src/mocks-projects/mock.front.js
+++ src/mocks-projects/mock.front.js
@@ -1,20 +1,20 @@
 export const mockFront = [
   {
     title: 'APP DE RECEITAS',
-    bgImage: "https://static.significados.com.br/foto/blog-og.jpg",
-    description: 'Desenvolvido uma API com Sequelize para a produção de conteúdo para um blog com Sequelize',
-    site: 'https://github.com/edmcorrea/Blogs-API---Back-End'
+    bgImage: "https://recursos.fitmoda.com.br/i/blog/receita-facil-torta-de-frango-1.jpg",
+    description: 'Para quem gosta de fazer comida gostosa!',
+    site: 'https://github.com/edmcorrea/project-app-de-receitas--'
   },
   {
-    title: 'STORY MANAGER',
-    bgImage: "https://br.mobiletransaction.org/wp-content/uploads/2020/07/dropshipping-brasil.jpg",
-    description: 'Desenvolvido um sistema de gerenciamento de vendas no formato dropshipping com MySQL',
-    site: 'https://github.com/edmcorrea/story-manager'
+    title: 'SOLAR SYSTEM',
+    bgImage: "https://cdn.mos.cms.futurecdn.net/v5n22xGyNNHLzSnSArbrVH.jpg",
+    description: 'Para os curiosos sobre os planetas e as missões espaciais',
+    site: 'https://github.com/edmcorrea/Solar-System-Project'
   },
   {
-    title: 'MEDIEVAL SHOP API',
-    bgImage: "https://medievalimago.org/wp-content/uploads/2020/05/6143991539_e1f2e3468d_o-1024x680.jpg",
-    description: 'Desenvolvido uma loja de itens medievais, no formato de uma API, utilizando Typescript e MySQL',
-    site: 'https://github.com/edmcorrea/medieval-shop'
+    title: 'SPOTUNES MUSICS',
+    bgImage: "http://www.meupositivo.com.br/doseujeito/wp-content/uploads/2021/03/historia-da-musica0.jpg",
+    description: 'Músicas de qualidade. Basta ouvir!',
+    site: 'https://github.com/edmcorrea/projeto-music-stream-Spotunes-'
   },
 ]
